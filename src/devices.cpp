317c5430cf58cbcef47e183c1b1b61f6a4492630
unknown <alexanderbroihier@gmail.com>
2021-05-18 13:01:48 -0500

    Updated devices.cpp
    Added a namespace motor_control section for subsystem 3 motors and sensors.

--- src/devices.cpp
+++ src/devices.cpp
@@ -1,5 +1,8 @@
 #include "drivetrain.hpp"
 
+/*
+ * Drivetrain Devices
+ */
 pros::Motor Drivetrain::frontLeftMotor  {12, false};
 pros::Motor Drivetrain::backLeftMotor   {18, false};
 pros::Motor Drivetrain::frontRightMotor {13, true};
@@ -10,3 +13,10 @@ pros::Imu Drivetrain::inertial {17};
 pros::ADIEncoder Drivetrain::leftEncoder    {'A', 'B', true};
 pros::ADIEncoder Drivetrain::rightEncoder   {'E', 'F', false};
 pros::ADIEncoder Drivetrain::middleEncoder  {'G', 'H', false};
+
+/*
+ * Non Drivetrain Devices
+ */
+namespace motor_control {
+
+} // namespace motor_control
