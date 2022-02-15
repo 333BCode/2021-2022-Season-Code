2c8b5f579771f8736d691f59e36dd9cda6f4e20f
Alex-D-B <79612371+Alex-D-B@users.noreply.github.com>
2022-02-15 12:58:32 -0600

    Added comments

--- include/util/pid_controller.hpp
+++ include/util/pid_controller.hpp
@@ -40,6 +40,11 @@ namespace motor_control {
 
         };
 
+        // constructor
+        // note the derivative gain (kD) comes before the integral gain (kI)
+        // integral cap limits the max voltage that the integral term can supply
+        // the last three parameters dictate slew control, and have units of volts/s^s, volts, and volts respectively
+        // if voltageAcceleration <= 0, slew control will not be used
         PIDController(long double kP, long double kD = 0, long double kI = 0, long double integralCap = 4,
             long double voltageAcceleration = 0, long double maxVoltage = 12, long double startingVoltage = 0);
 
@@ -48,41 +53,62 @@ namespace motor_control {
         // returns the derivative of the system when calc power was last run
         long double getDerivative();
 
+        // resets the PIDController and sets a new target
+        // if ignoreProfile is true, slew is disabled until the next reset, otherwise slew is enabled (if voltageAcceleration > 0)
         void setNewTarget(long double newTarget, bool ignoreProfile = false);
         // changes the target, does not reset slew, derivative, or integral terms
         void alterTarget(long double newTarget);
 
         // inform the PIDController of updates to the system's output since calcPower was last ran (example: after a motion that does not utilize PID)
+        // units are millivolts for consistency with calcPower
         void updatePreviousSystemOutput(int previousSystemOutput);
 
+        // returns a suggestion for the millivolts to supply to the system, based off of the current position of said system
+        // units are millivolts because pros::Motor::move_voltage uses millivolts
         int calcPower(long double currPos);
 
+        // returns the current gains for both PID and slew
         Constants getConstants();
+        // sets the PID gains
+        // note the derivative gain (d) comes before the integral gain (i)
         void setConstants(long double p, long double d, long double i, long double integralCap);
+        // sets the slew constants with units of volts/s^s, volts, and volts respectively
+        // if voltageAcceleration <= 0, slew control will not be used
         void setSlewConstants(long double newVoltageAcceleration, long double newMaxVoltage, long double newStartingVoltage);
 
     private:
 
+        // PID gains
         long double kP;
         long double kD;
         long double kI;
         long double integralCap;
 
+        // positional data for the system
         long double target      {0};
         long double error       {0};
         long double prevError   {0};
         long double derivative  {0};
         long double totalError  {0};
 
+        // slew constants
         long double voltageAcceleration;
         long double maxVoltage;
         long double startingVoltage;
+        // stores the current power to supply due to slew control
         long double slewPower   {0};
+        // stores the time at which calcPower was last run (for derivative, integral, and slew calculations)
         uint32_t startTime      {0};
 
+        // state of whether slew control should be used / calculated
         bool usingSlew  {false};
+        // state of whether calcPower is being called for the first time after a reset
+        // When true, the slew profile is given direction,
+        // and the previous output will not override the output (which would normally happen if dT between calcPower calls is 0ms)
         bool firstRun   {true};
 
+        // stores the value of the previous output from the system (either from calcPower or from information supplied by updatePreviousSystemOutput)
+        // used when dT between calcPower calls is 0ms and to properly reset the slew profile during a setNewTarget call
         long double previousOutput {0};
 
     };
